f828c11c48eaceb2cacc7a2006d6de6e5a850aed
GustavoOliveira <gustavo.oliveira@hover.to>
2023-05-03 00:06:59 -0700

    Update comments_distribution.sql

--- comments_distribution.sql
+++ comments_distribution.sql
@@ -1,24 +1,24 @@
-Write a query to calculate the distribution of comments by the count of users that joined Meta/Facebook between 2018 and 2020, for the month of January 2020.
+-- Write a query to calculate the distribution of comments by the count of users that joined Meta/Facebook between 2018 and 2020, for the month of January 2020.
 
-The output should contain a count of comments and the corresponding number of users that made that number of comments in Jan-2020. For example, you'll be counting how many users made 1 comment, 2 comments, 3 comments, 4 comments, etc in Jan-2020. Your left column in the output will be the number of comments while your right column in the output will be the number of users. Sort the output from the least number of comments to highest.
+-- The output should contain a count of comments and the corresponding number of users that made that number of comments in Jan-2020. For example, you'll be counting how many users made 1 comment, 2 comments, 3 comments, 4 comments, etc in Jan-2020. Your left column in the output will be the number of comments while your right column in the output will be the number of users. Sort the output from the least number of comments to highest.
 
-To add some complexity, there might be a bug where an user post is dated before the user join date. You'll want to remove these posts from the result.
+-- To add some complexity, there might be a bug where an user post is dated before the user join date. You'll want to remove these posts from the result.
 
-fb_users
+-- fb_users
 
-id: int
-name: varchar
-joined_at: datetime
-city_id: int
-device: int
+-- id: int
+-- name: varchar
+-- joined_at: datetime
+-- city_id: int
+-- device: int
 
-fb_comments
+-- fb_comments
 
-user_id: int
-body: varchar
-created_at: datetime
+-- user_id: int
+-- body: varchar
+-- created_at: datetime
 
-Solution:
+-- Solution:
 
 select
   numb_comments,
