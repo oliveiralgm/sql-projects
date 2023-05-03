e0727167e25f1edb7728fccbe3ebaf166446886d
GustavoOliveira <goliveir@syr.edu>
2023-05-03 00:23:20 -0700

    Update days_at_numb_one.sql

--- days_at_numb_one.sql
+++ days_at_numb_one.sql
@@ -1,5 +1,25 @@
-Find the number of days a US track has stayed in the 1st position for both the US and worldwide rankings. Output the track name and the number of days in the 1st position. Order your output alphabetically by track name.
-If the region 'US' appears in dataset, it should be included in the worldwide ranking.
+-- Find the number of days a US track has stayed in the 1st position for both the US and worldwide rankings. Output the track name and the number of days in the 1st position. Order your output alphabetically by track name.
+-- If the region 'US' appears in dataset, it should be included in the worldwide ranking.
+
+-- spotify_daily_rankings_2017_us
+
+-- position:int
+-- trackname:varchar
+-- artist:varchar
+-- streams:int
+-- url:varchar
+-- date:datetime
+
+-- spotify_worldwide_daily_song_ranking
+
+-- id:int
+-- position:int
+-- trackname:varchar
+-- artist:varchar
+-- streams:int
+-- url:varchar
+-- date:datetime
+-- region:varchar
 
 
 with pos_us as
