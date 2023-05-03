55ea1ea863acecc0407e251f6d7e4561c337d09c
GustavoOliveira <gustavo.oliveira@hover.to>
2023-05-03 00:02:34 -0700

    Create top_percentile_fraud.sql

---
+++ top_percentile_fraud.sql
@@ -0,0 +1,30 @@
+ABC Corp is a mid-sized insurer in the US and in the recent past their fraudulent claims have increased significantly for their personal auto insurance portfolio. 
+They have developed a ML based predictive model to identify propensity of fraudulent claims. 
+Now, they assign highly experienced claim adjusters for top 5 percentile of claims identified by the model.
+Your objective is to identify the top 5 percentile of claims from each state. 
+Your output should be policy number, state, claim cost, and fraud score.
+
+fraud_score
+
+policy_num: varchar
+state: varchar
+claim_cost: int
+fraud_score: float
+
+-- Solution
+with percentile as (
+select 
+    policy_num
+    ,state
+    ,claim_cost
+    ,fraud_score
+    ,percent_rank() over (partition by state order by fraud_score) as percentile_fraud
+from fraud_score
+)
+select
+    policy_num
+    ,state
+    ,claim_cost
+    ,fraud_score
+from percentile
+where percentile_fraud >= 0.95
