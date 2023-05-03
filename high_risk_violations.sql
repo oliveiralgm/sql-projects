b94b192a785083eb0aeec6d6ed0030c231e10cc6
GustavoOliveira <goliveir@syr.edu>
2023-05-03 00:15:06 -0700

    Update high_risk_violations.sql

--- high_risk_violations.sql
+++ high_risk_violations.sql
@@ -1,25 +1,25 @@
-Find details of the business with the highest number of high-risk violations. 
-Output all columns from the dataset considering business_id which consist 'high risk' phrase in risk_category column.
+-- Find details of the business with the highest number of high-risk violations. 
+-- Output all columns from the dataset considering business_id which consist 'high risk' phrase in risk_category column.
 
-sf_restaurant_health_violations
+-- sf_restaurant_health_violations
 
-business_id:int
-business_name:varchar
-business_address:varchar
-business_city:varchar
-business_state:varchar
-business_postal_code:float
-business_latitude:float
-business_longitude:float
-business_location:varchar
-business_phone_number:float
-inspection_id:varchar
-inspection_date:datetime
-inspection_score:float
-inspection_type:varchar
-violation_id:varchar
-violation_description:varchar
-risk_category:varchar
+-- business_id:int
+-- business_name:varchar
+-- business_address:varchar
+-- business_city:varchar
+-- business_state:varchar
+-- business_postal_code:float
+-- business_latitude:float
+-- business_longitude:float
+-- business_location:varchar
+-- business_phone_number:float
+-- inspection_id:varchar
+-- inspection_date:datetime
+-- inspection_score:float
+-- inspection_type:varchar
+-- violation_id:varchar
+-- violation_description:varchar
+-- risk_category:varchar
 
 
 
