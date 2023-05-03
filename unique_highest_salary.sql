134cde2b221c32774af2a04558355446630fb803
GustavoOliveira <gustavo.oliveira@hover.to>
2023-05-03 00:03:43 -0700

    Update unique_highest_salary.sql

--- unique_highest_salary.sql
+++ unique_highest_salary.sql
@@ -1,21 +1,21 @@
-Find the highest salary among salaries that appears only once.
+-- Find the highest salary among salaries that appears only once.
 
-employee
+-- employee
 
-id: int
-first_name: varchar
-last_name: varchar
-age: int
-sex: varchar
-employee_title: varchar
-department: varchar
-salary: int
-target: int
-bonus: int
-email: varchar
-city: varchar
-address: varchar
-manager_id: int
+-- id: int
+-- first_name: varchar
+-- last_name: varchar
+-- age: int
+-- sex: varchar
+-- employee_title: varchar
+-- department: varchar
+-- salary: int
+-- target: int
+-- bonus: int
+-- email: varchar
+-- city: varchar
+-- address: varchar
+-- manager_id: int
 
 select
 max(distinct salary)
