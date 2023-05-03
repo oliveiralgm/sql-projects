567049b11698e3092ab97ea35003c734ac00c919
GustavoOliveira <gustavo.oliveira@hover.to>
2023-05-03 00:10:46 -0700

    Update revenue_over_time.sql

--- revenue_over_time.sql
+++ revenue_over_time.sql
@@ -1,11 +1,11 @@
-Find the 3-month rolling average of total revenue from purchases given a table with users, their purchase amount, and date purchased. Do not include returns which are represented by negative purchase values. Output the year-month (YYYY-MM) and 3-month rolling average of revenue, sorted from earliest month to latest month. 
-A 3-month rolling average is defined by calculating the average total revenue from all user purchases for the current month and previous two months. The first two months will not be a true 3-month rolling average since we are not given data from last year. Assume each month has at least one purchase.
+-- Find the 3-month rolling average of total revenue from purchases given a table with users, their purchase amount, and date purchased. Do not include returns which are represented by negative purchase values. Output the year-month (YYYY-MM) and 3-month rolling average of revenue, sorted from earliest month to latest month. 
+-- A 3-month rolling average is defined by calculating the average total revenue from all user purchases for the current month and previous two months. The first two months will not be a true 3-month rolling average since we are not given data from last year. Assume each month has at least one purchase.
 
-amazon_purchases
+-- amazon_purchases
 
-user_id: int
-created_at: datetime
-purchase_amt: int
+-- user_id: int
+-- created_at: datetime
+-- purchase_amt: int
 
 -- turn date into mmmm-yy
 -- sum over date
